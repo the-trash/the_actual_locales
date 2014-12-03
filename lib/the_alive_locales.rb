de7964e8c8a0ebe386088e8ec9099c88a4572894
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-12-03 13:13:45 +0300

    first commit

--- lib/the_alive_locales.rb
+++ lib/the_alive_locales.rb
@@ -1,5 +1,60 @@
 require "the_alive_locales/version"
 
 module TheAliveLocales
-  # Your code goes here...
+  def self.log_path locale
+    "#{ Rails.root }/log/alive_locale.#{ locale }.yml"
+  end
+
+  def self.dump locale, path, value
+    # Create alive file if not exists
+    log_path = self.log_path(locale)
+    File.open(log_path, 'w+').close unless File.exists? log_path
+
+    # Open and read alive locale file
+    i18n_log = File.open(log_path, "r")
+    log_content = i18n_log.read
+    i18n_log.close
+
+    # Read YAML
+    # Create { 'en' => {} } if exception
+    locale_data = begin; YAML.load(log_content); rescue; {}; end
+    locale_data[locale] = {} unless locale_data[locale]
+
+    # Set base values
+    path_size     = path.size - 1
+    current_level = locale_data[locale]
+
+    # Set base values
+    path.each_with_index do |v, i|
+      _value = (path_size == i) ? value : {}
+      current_level[v.to_s] = _value unless current_level[v.to_s]
+      current_level = current_level[v.to_s]
+    end
+
+    # Open alive locale file and write
+    i18n_log = File.open(log_path, 'w')
+    i18n_log.write locale_data.to_yaml
+    i18n_log.close
+  end
+
+  class Engine < ::Rails::Engine
+    initializer 'the-alive-locales.register' do
+      module I18n
+        def self.translate(*args)
+          tname = args.try :[], 0
+          _args = args.try :[], 1
+          scope = Array.wrap _args.try(:[],:scope) || _args.try(:[], 'scope')
+
+          key   = [ scope, tname ].flatten
+          value = super
+
+          TheAliveLocales.dump(locale.to_s, key, value)
+
+          value
+        end
+
+        self.singleton_class.send(:alias_method, :t, :translate)
+      end
+    end
+  end
 end
--- lib/the_alive_locales/version.rb
+++ lib/the_alive_locales/version.rb
@@ -1,3 +1,1 @@
-module TheAliveLocales
-  VERSION = "0.0.1"
-end
+require_relative '../../gem_version.rb'
